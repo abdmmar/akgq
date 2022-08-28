4f2e08517df11cd0514f33a2ba0ade9e41c84676
abdmmar <opowae909@gmail.com>
2022-08-28 12:20:33 +0700

    feat: use theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from '../styles/globals'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
