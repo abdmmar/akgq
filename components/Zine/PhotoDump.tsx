c77ede6e37ebb928a3e68f9fff7fb20bab22527a
abdmmar <opowae909@gmail.com>
2022-09-06 14:37:15 +0700

    feat: add photo dump

--- components/Zine/PhotoDump.tsx
+++ components/Zine/PhotoDump.tsx
@@ -1,25 +1,57 @@
 import styled from 'styled-components'
+import NextImage from 'next/image'
 import _Container from '../Common/Container'
 
 const PhotoDump = () => {
   return (
     <Container>
       <Wrapper>
-        <H2>Photo Dump</H2>
+        <Header>
+          <H2>Photo Dump</H2>
+        </Header>
+        {Array(8)
+          .fill(1)
+          .map((_, i) => (
+            <NextImage
+              key={i + 1}
+              src={`/images/photodump/${i + 1}.png`}
+              layout="responsive"
+              objectFit="cover"
+              loading="lazy"
+              width={1953}
+              height={1323}
+            />
+          ))}
       </Wrapper>
     </Container>
   )
 }
 
 const Container = styled(_Container)`
+  padding: 0;
   background-color: ${(props) => props.theme.colors.midnight};
   color: ${(props) => props.theme.colors.white};
 `
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 52px;
+  width: 100%;
+`
+
+const Header = styled(_Container)`
+  min-height: unset;
+  padding: 20px 150px;
+
+  @media screen and (max-width: 1024px) {
+    padding: 20px 100px;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 20px 50px;
+  }
+
+  @media screen and (max-width: 425px) {
+    padding: 20px 20px;
+  }
 `
 
 const H2 = styled.h2`
